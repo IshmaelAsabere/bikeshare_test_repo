52e048efdf3bc768583514c1f11279ea29f7984e
Ishmael Asabere <ishmael.asabere@gmail.com>
2023-01-09 17:05:35 +0000

    making changes in the branch A

--- data_preparation.py
+++ data_preparation.py
@@ -1,28 +1,12 @@
+# Import relevant libraries
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+# Import data
 games = pd.read_csv('./data/appstore_games.csv')
 
-original_colums_dict = {x: x.lower().replace(' ','_') for x in games.columns}
+# Preview the data
+games.head()
 
-games.rename(
-    columns = original_colums_dict,
-    inplace = True
-)
-
-games.set_index(
-    keys = 'id',
-    inplace = True
-)
-
-games.drop(
-    columns = ['url', 'icon_url'],
-    inplace = True
-)
-
-games['original_release_date'] = pd.to_datetime(games['original_release_date'])
-games['current_version_release_date'] = pd.to_datetime(games['current_version_release_date'])
-
-games = games.loc[games['average_user_rating'].notnull()]
-
-games = games.loc[games['user_rating_count'] >= 30]
